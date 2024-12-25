a34e56a384a7e54c7208fa64cbc3668b54f8abf5
samsonajulor <samsonajulor@gmail.com>
2024-12-26 00:27:32 +0100

    separted concerns

---
+++ src/sdk.ts
@@ -0,0 +1,33 @@
+// ===========================
+// SDK Definitions
+// ===========================
+export { QuikDB } from './controllers';
+export {
+  Field,
+  CreateSchemaArgs,
+  ResultBool,
+  GetSchemaArgs,
+  Schema,
+  DeleteSchemaArgs,
+  ListSchemasArgs,
+  InsertDataArgs,
+  GetRecordArgs,
+  GetAllRecordsArgs,
+  UpdateDataArgs,
+  DeleteDataArgs,
+  GetOwnerArgs,
+  InitOwnerArgs,
+  GetMetricsArgs,
+  GetRecordSizesArgs,
+  QueryByIndexArgs,
+  SearchByIndexArgs,
+  SearchByMultipleFieldsArgs,
+  DBRecord,
+  NoOfSchemaArgs,
+  ResultRecords,
+  ResultString,
+  ResultStrings,
+  ResultTuple,
+  CanisterMethod,
+} from './@types';
+export  { Principal } from '@dfinity/principal';
