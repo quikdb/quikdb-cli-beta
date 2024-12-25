cb04e0fd891a648143a595679044f48f667f13cb
samsonajulor <samsonajulor@gmail.com>
2024-12-26 00:28:36 +0100

    ignoring v1

--- v1/index.d.ts
+++ v1/index.d.ts
@@ -1,4 +1,2 @@
 #!/usr/bin/env node
-export { QuikDB } from '../src/controllers';
-export { Field, CreateSchemaArgs, ResultBool, GetSchemaArgs, Schema, DeleteSchemaArgs, ListSchemasArgs, InsertDataArgs, GetRecordArgs, GetAllRecordsArgs, UpdateDataArgs, DeleteDataArgs, GetOwnerArgs, InitOwnerArgs, GetMetricsArgs, GetRecordSizesArgs, QueryByIndexArgs, SearchByIndexArgs, SearchByMultipleFieldsArgs, DBRecord, NoOfSchemaArgs, ResultRecords, ResultString, ResultStrings, ResultTuple, CanisterMethod, } from '../src/@types';
-export { Principal } from '@dfinity/principal';
+export {};
