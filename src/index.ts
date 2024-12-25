867e10db9429b92ee7e3e646c3aa941fbfc53986
samsonajulor <samsonajulor@gmail.com>
2024-12-25 23:46:30 +0100

    testing sdk

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,37 @@
 'use strict';
 import { program } from './programs';
 program.parse(process.argv);
+
+// ===========================
+// SDK Definitions
+// ===========================
+export { QuikDB } from '../src/controllers';
+export {
+  Field,
+  CreateSchemaArgs,
+  ResultBool,
+  GetSchemaArgs,
+  Schema,
+  DeleteSchemaArgs,
+  ListSchemasArgs,
+  InsertDataArgs,
+  GetRecordArgs,
+  GetAllRecordsArgs,
+  UpdateDataArgs,
+  DeleteDataArgs,
+  GetOwnerArgs,
+  InitOwnerArgs,
+  GetMetricsArgs,
+  GetRecordSizesArgs,
+  QueryByIndexArgs,
+  SearchByIndexArgs,
+  SearchByMultipleFieldsArgs,
+  DBRecord,
+  NoOfSchemaArgs,
+  ResultRecords,
+  ResultString,
+  ResultStrings,
+  ResultTuple,
+  CanisterMethod,
+} from '../src/@types';
+export  { Principal } from '@dfinity/principal';
